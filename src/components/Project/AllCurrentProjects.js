647f9c9596e6640f7bdb3e6d5f2bd681fc92d539
Annaliese Mathews <amesibov043@gmail.com>
2023-04-03 15:19:55 -0500

    Only patterns open in a new tab. clicking on a project name or img keeps the user in the same tab.

--- src/components/Project/AllCurrentProjects.js
+++ src/components/Project/AllCurrentProjects.js
@@ -38,7 +38,7 @@ export const AllCurrentProjects = () => {
                                 </div>
                                 <div className="card-content">
                                 <div className="media-content">
-                                    <Link to={`/projects/${project.id}`} target="_blank" rel="noreferrer noopener"> <p className="title is-4">{project.name}</p> </Link>
+                                    <Link to={`/projects/${project.id}`}> <p className="title is-4">{project.name}</p> </Link>
                                     <Link to={project.patternURL} target="_blank" rel="noreferrer noopener"> Link to Pattern </Link> 
                                     <p>Level:{project?.level?.level}</p>
                                 </div>
@@ -53,7 +53,7 @@ export const AllCurrentProjects = () => {
                            </div>
                            <div className="card-content">
                            <div className="media-content">
-                               <Link to={`/projects/${project.id}`} target="_blank" rel="noreferrer noopener"> <p className="title is-4">{project.name}</p> </Link>
+                               <Link to={`/projects/${project.id}`}> <p className="title is-4">{project.name}</p> </Link>
                                <p>Level:{project?.level?.level}</p>
                            </div>
 
@@ -66,7 +66,7 @@ export const AllCurrentProjects = () => {
                                <div className="card-content">
                                <div className="media-content">
                                     <button onClick={() => navigate(`/edit/${project.id}`)} className="button is-link btn" > Add An inspiration Picture for this Project </button>
-                                   <Link to={`/projects/${project.id}`} target="_blank" rel="noreferrer noopener"> <p className="title is-4">{project.name}</p> </Link>
+                                   <Link to={`/projects/${project.id}`}> <p className="title is-4">{project.name}</p> </Link>
                                    <Link to={project.patternURL} target="_blank" rel="noreferrer noopener"> Link to Pattern </Link> 
                                    <p>Level: {project?.level?.level}</p>
                                </div>
@@ -79,7 +79,7 @@ export const AllCurrentProjects = () => {
                                <div className="card-content">
                                <div className="media-content">
                                     <button onClick={() => navigate(`/edit/${project.id}`)} className="button is-link btn" > Add A Link to Your Pattern or An Inspiration Picture for this Project </button>
-                                   <Link to={`/projects/${project.id}`} target="_blank" rel="noreferrer noopener"> <p className="title is-4">{project.name}</p> </Link>
+                                   <Link to={`/projects/${project.id}`}> <p className="title is-4">{project.name}</p> </Link>
                                    <p>Level: {project?.level?.level}</p>
                                </div>
 
